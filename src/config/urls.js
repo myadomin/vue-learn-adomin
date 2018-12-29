adcc91cd7384d3be8ee0b6500a2424a27c4bc1b9
luominmin <luominmin@qtrade.com.cn>
2018-12-29 17:57:01 +0800

    some change

--- src/config/urls.js
+++ src/config/urls.js
@@ -8,6 +8,9 @@ const serverDict = {
 const ctx = serverDict[SERVER]
 /*eslint-disable*/
 
+// localhost通过webpack dev-server proxy转发到http://localhost:3001
+// npm run build后 自动加上ctx, serverDict.test或者serverDict.prod, 请求3001或者3002
+
 export default {
   product_all: '/product/all',
   product_buy: '/product/buy',
